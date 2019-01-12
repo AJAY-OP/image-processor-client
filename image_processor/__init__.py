8966ff18cc92e66a6b0b556da4b49284902e5ff8
thecosmos <deepakrajko14@gmail.com>
2019-01-13 01:16:26 +0530

    Alpha package

--- image_processor/__init__.py
+++ image_processor/__init__.py
@@ -1,1 +1,6 @@
 from .client import Client
+
+__version__ = "0.1.0"
+__author__ = "□ | The Cosmos"
+
+__all__ = ["Client"]
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requirements = [requirement for requirement in open("requirements.txt").read().splitlines()]
+
+setup(
+    name="image_processor",
+    version="0.1.0",
+    url="https://github.com/thec0sm0s/image-processor-client",
+    author="□ | The Cosmos",
+    description="Asynchronous python client for image-processor server (https://github.com/thec0sm0s/image-processor).",
+    long_description=open("README.md").read(),
+    packages=['image_processor'],
+    install_requires=requirements,
+    classifiers=[
+            'Development Status :: 2 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Image',
+            'Topic :: Tools',
+    ]
+)
