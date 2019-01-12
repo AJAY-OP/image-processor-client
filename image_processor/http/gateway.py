b855c8a4c432137a0490fff8e79b3e5a839070a2
thecosmos <deepakrajko14@gmail.com>
2019-01-13 00:39:23 +0530

    Ignore the warning, create session outside async

--- image_processor/http/gateway.py
+++ image_processor/http/gateway.py
@@ -11,12 +11,9 @@ class HttpImageClient(ImageFunctions):
     def __init__(self, uri: str = None, loop=None):
         self.session: aiohttp.ClientSession = None
         self.loop = loop or asyncio.get_event_loop()
-        self.loop.run_until_complete(self.__get_http_session())
+        self.session = aiohttp.ClientSession(loop=self.loop)
         Route.BASE_URL = uri or Route.BASE_URL
         self._route = Route
-
-    async def __get_http_session(self):
-        self.session = aiohttp.ClientSession(loop=self.loop)
 
     def route(self, path, method: str = "POST", **params):
         return self._route(path, method, **params)
