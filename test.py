87219662a70553cff3941f37765d5a910ff2ccbb
thecosmos <deepakrajko14@gmail.com>
2019-01-17 22:18:27 +0530

    Import fix in python tests

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+import sys
+
 import unittest
 from typing import Union
 
 from tests.client import ImageClientTest
 from tests.methods import DiscordMethodsTest
 
+sys.path.append("image_processor")
 
 if __name__ == "__main__":
     unittest.main(Union[DiscordMethodsTest()])
