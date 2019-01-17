2d188ca6b5496bfeb440ac7aa9eb3e44ba5f13b4
thecosmos <deepakrajko14@gmail.com>
2019-01-17 23:17:19 +0530

    Import fix in python tests

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-import sys
-
 import unittest
 from typing import Union
 
 from tests.client import ImageClientTest
 from tests.methods import DiscordMethodsTest
 
-sys.path.append("./image_processor/")
-
 if __name__ == "__main__":
-    unittest.main(Union[DiscordMethodsTest()])
     unittest.main(Union[ImageClientTest()])
+    unittest.main(Union[DiscordMethodsTest()])
